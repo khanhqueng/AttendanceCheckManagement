879ce8225fcd4801fa3bf0effe59b5c74ed29bae
khanhqueng <quangkhanh288123@gmail.com>
2024-12-09 16:03:38 +0700

    hot fix

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,10 +6,10 @@ VALUES
 ON DUPLICATE KEY UPDATE
 id = VALUES(id);
 
-INSERT INTO user (id, role_id, password, username)
+INSERT IGNORE INTO user (id, role_id, password, username)
 VALUES (2, 1, '$2a$10$7m9bdD49Aei2O0SVHY9Tge5P4MiyKbv6V4EFsDQaNWXBxpmzxJoQG', 'manager');
 
-INSERT INTO manager (id, dob, user_id, email, name, phone_number)
+INSERT IGNORE INTO manager (id, dob, user_id, email, name, phone_number)
 VALUES (1, '1990-05-15', 2, 'manager@gm.uit.edu.vn', 'Nguyen Thi Hoa', '0964321467');
 
 
