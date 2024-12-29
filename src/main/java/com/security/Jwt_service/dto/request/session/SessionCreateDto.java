6f34e528ac521a63b03060ce155152ba213ebcbd
khanhqueng <quangkhanh288123@gmail.com>
2024-12-29 21:20:35 +0700

    add rollcal api

--- src/main/java/com/security/Jwt_service/dto/request/session/SessionCreateDto.java
+++ src/main/java/com/security/Jwt_service/dto/request/session/SessionCreateDto.java
@@ -1,6 +1,7 @@
 package com.security.Jwt_service.dto.request.session;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -19,11 +20,17 @@ import java.time.LocalTime;
 public class SessionCreateDto {
     @NotNull(message = "number of sessions must not be null")
     private int numberSessions;
-    @JsonFormat(pattern = "HH:mm")
+
     @NotNull(message = "on-class time must not be null")
+    @JsonFormat(pattern = "HH:mm")
+    @Schema(description = "The time the class starts, formatted as HH:mm", example = "16:30")
     private LocalTime onClassTime;
+
     @NotNull(message = "end time must not be null")
+    @JsonFormat(pattern = "HH:mm")
+    @Schema(description = "The time the class starts, formatted as HH:mm", example = "16:30")
     private LocalTime endClassTime;
+
     @NotNull(message = "classroom id must not be null")
     private Long classRoomId;
 }
